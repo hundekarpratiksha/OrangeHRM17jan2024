f52d5b76870d42263da4634e7ab74a4b90fa4cb8
unknown <hundekarpratiksha@gmail.com>
2024-01-17 16:34:45 +0530

    Orang hrm BDD Framework created

---
+++ AllFeatureFile/OrangeHrm.feature
@@ -0,0 +1,6 @@
+Feature: Test Orange HRM Application
+
+  Scenario: Test Login Page Functionality
+    Given user is on login page
+    When user enter valid creentials
+    And user click on login button
---
+++ src/test/java/StepDefination/LoginPageStep.java
@@ -0,0 +1,21 @@
+package StepDefination;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
+public class LoginPageStep {
+	
+	@Given("user is on login page")
+	public void user_is_on_login_page() {
+	    
+	}
+	@When("user enter valid creentials")
+	public void user_enter_valid_creentials() {
+		
+	}
+	@When("user click on login button")
+	public void user_click_on_login_button() {
+	    
+	}
+
+}
